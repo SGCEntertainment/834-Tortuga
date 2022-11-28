caab7d959a815b21dc3a764abe858c8aa940dcfe
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-11-28 15:04:24 +0300

    update

--- Assets/834 Tortuga/Scripts/Managers/GameManager.cs
+++ Assets/834 Tortuga/Scripts/Managers/GameManager.cs
@@ -5,33 +5,54 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get => FindObjectOfType<GameManager>(); }
 
-
-
-    private int Score { get; set; }
+    private bool GameStarted { get; set; }
+    private float GameSpeed { get; set; } = 6.0f;
+    public float Score { get; set; }
     private Player Player { get; set; }
 
 
-
     public static Action OnHandlePulled { get; set; } = delegate { };
-    public static Action<int> OnGameFinsihed { get; set; } = delegate { };
+    public static Action<float> OnGameFinsihed { get; set; } = delegate { };
 
     private void Awake()
     {
         Player = Resources.Load<Player>("player");
     }
 
-    private void Start()
+    private void Update()
     {
-        RestartGame();
+        if(!GameStarted)
+        {
+            return;
+        }
+
+        Score += GameSpeed * Time.deltaTime;
     }
 
     public void RestartGame()
     {
+        Score = 0;
+
         Instantiate(Player, GameObject.Find("Environment").transform).transform.position = new Vector2(0, -3.47f);
+        GameStarted = true;
     }
 
     public void GameOver()
     {
         OnGameFinsihed?.Invoke(Score);
+
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+        foreach(Obstacle ob in obstacles)
+        {
+            Destroy(ob.gameObject);
+        }
+
+        GameStarted = false;
+    }
+
+    public void ExitToMenu()
+    {
+        Destroy(FindObjectOfType<Player>().gameObject);
+        GameStarted = false;
     }
 }
